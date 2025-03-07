3db857033409e45e65ba409afce21f93e9b2daf2
jabteepabuser <djoplo1@wgu.edu>
2025-03-06 22:40:20 -0600

    first commit

---
+++ src/app/myheader.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";   
+
+@Component({
+    selector: 'my-header',     //which element should be replaced or controlled in this case, it is my-header.
+    templateUrl: './myheader.component.html' //the template for this component which basically means the file that contains the markup for this comp.
+})
+
+export class MyHeaderComponent {}
