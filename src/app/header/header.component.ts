7d6fe9515593fe3636ff86c8f9c7c3ae51a4eb9e
jabteepabuser <djoplo1@wgu.edu>
2025-03-10 22:56:21 -0500

    final cleanup

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";   //import this always to ts, when creating new components.
 
 @Component({
-    selector: 'header-app',     //which element should be replaced or controlled in this case, it is my-header.
+    selector: 'app-header',     //which element should be replaced or controlled in this case, it is my-header.
     standalone: true, //should always be here.
     templateUrl: './header.component.html', //the template for this component which basically means the file that contains the markup for this comp.
     styleUrl: './header.component.css' //the attaches the styling component.
